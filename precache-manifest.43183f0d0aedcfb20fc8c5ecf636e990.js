af22c2c6196ee2c01fb499332b897125959ef31d
munshkr <munshkr@users.noreply.github.com>
2021-05-03 22:55:02 +0000

    deploy: 0298d3acbfe4abc65ac657bcd0f5f1af9198e822

---
+++ precache-manifest.43183f0d0aedcfb20fc8c5ecf636e990.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d326b0f61ddb09b218d64946bfa18b7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e4c9568442d3e18ccd1",
+    "url": "/static/css/main.678ea0ff.chunk.css"
+  },
+  {
+    "revision": "a515617352daa6dbf553",
+    "url": "/static/js/2.f296c343.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/static/js/2.f296c343.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e4c9568442d3e18ccd1",
+    "url": "/static/js/main.cc081b47.chunk.js"
+  },
+  {
+    "revision": "520983a5d79efb58a64a",
+    "url": "/static/js/runtime-main.0603d622.js"
+  }
+]);
