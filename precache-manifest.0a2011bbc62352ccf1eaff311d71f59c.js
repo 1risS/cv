42d55bbb67327cb28a769c44071b497fc8c66c59
1risS <iris.r.saladino@gmail.com>
2021-05-03 18:15:38 -0300

    Updates

---
+++ precache-manifest.0a2011bbc62352ccf1eaff311d71f59c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a672ce9ac838a0f14b86eccb6dca15b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03b256fe7bab2a5f2dd3",
+    "url": "/static/css/main.678ea0ff.chunk.css"
+  },
+  {
+    "revision": "a515617352daa6dbf553",
+    "url": "/static/js/2.f296c343.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/static/js/2.f296c343.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03b256fe7bab2a5f2dd3",
+    "url": "/static/js/main.3df061e2.chunk.js"
+  },
+  {
+    "revision": "520983a5d79efb58a64a",
+    "url": "/static/js/runtime-main.0603d622.js"
+  }
+]);
